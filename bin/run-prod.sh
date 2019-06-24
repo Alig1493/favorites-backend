799ecb8f69a366f574f2ce62ead6d1344b68feed
alig1493 <zubair@kajkey.com>
2019-06-24 15:15:05 +0600

    Added loaddata to run-prod.sh

--- bin/run-prod.sh
+++ bin/run-prod.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 urlwait &&
 ./manage.py migrate &&
+./manage.py loaddata categories.json &&
+./manage.py loaddata users.json &&
 gunicorn -w 1 --access-logfile=- --timeout=120 backend.wsgi:application --bind 0.0.0.0:5000
