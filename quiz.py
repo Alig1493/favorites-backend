fdf746aabf9dd61ab1f25413058a35ab3de6498b
alig1493 <zubair@kajkey.com>
2019-07-19 10:42:11 +0600

    Added quiz.py

--- quiz.py
+++ quiz.py
@@ -1,17 +1,14 @@
 from cryptography.fernet import Fernet
 
-key = "TluxwB3fV_GWuLkR1_BzGs1Zk90TYAuhNMZP_0q4WyM="
+key = 'TluxwB3fV_GWuLkR1_BzGs1Zk90TYAuhNMZP_0q4WyM='
 
 # Oh no! The code is going over the edge! What are you going to do?
-# Break it into multiple lines to fit the standards
-message = b"gAAAAABc-TXUyheBme9ilvLbUsFnUZrSe7oucsmuGuEVqJ0fGLJ1aNrrx2wV-" \
-          b"DLOSYksmf54qhs926SSbi4ui4xnVhZbTb65JteEpldDXKgoUnTKyFs_" \
-          b"C28aIsZtNh7aOvA886GgEMqU7tPptN20VvkmKYsdjqq5Qlk7ELMdhu-oNeyZv6IRkTY="
+message = b'gAAAAABdMUmDKU5-UPGK0VcKnsSl6nFBrCBYHaj94IbW2YZuo2BwveVuTkLkyrYicb-JiIDIk2H24R_Ukmyg2aD1GgxSydpcJF5YyO8a6uZsZB1o6622HI0ZfNUQwf-Yoo_zWXMiseYMeGBoSgaAg9yvuBtjWfmyCGov3RXzfZmHCFNIdXKwCNo='
 
 
 def main():
     f = Fernet(key)
-    print(f.decrypt(message).decode("utf-8"))
+    print(f.decrypt(message))
 
 
 if __name__ == "__main__":
